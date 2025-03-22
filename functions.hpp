09ea5096bbf0e260272c4f05580627fa5befb0cf
WOLF <blackwolf3466@gmail.com>
2025-03-22 11:30:07 +0200

    Arranged functions

---
+++ functions.hpp
@@ -0,0 +1,5 @@
+void drawBoard(char *spaces);
+void playerMove(char *spaces, char player);
+void computerMove(char *spaces, char computer);
+bool checkWinner(char *spaces, char player, char computer);
+bool checkTie(char *spaces);
